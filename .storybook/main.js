36535606f3e461276b61a8e8a402d4f550a83538
Louis Gollut <louis.gollut@gmail.com>
2020-04-14 00:38:11 +0200

    Add main configuration to storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,12 @@
 module.exports = {
-  stories: ["../src/**/*.stories.[tj]s"],
-  addons: ["@storybook/addon-actions", "@storybook/addon-links"],
+  stories: ['../src/Introduction.stories.mdx', '../src/**/*.stories.(js|mdx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-docs',
+    '@storybook/addon-storysource',
+    '@storybook/addon-viewport/register',
+    '@storybook/addon-contexts/register',
+    '@storybook/addon-toolbars',
+    '@storybook/addon-actions',
+  ],
 };
