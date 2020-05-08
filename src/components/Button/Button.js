ed9afdf59a1afd08598f5e3f5c0a3a4ffe4404e5
Louis Gollut <louis.gollut@gmail.com>
2020-05-08 09:17:17 +0200

    Refactor Button component

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,25 +1,62 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button as TuiButton } from '@theme-ui/components';
+import { Button as TuiButton, Text } from 'theme-ui';
+import { lighten, alpha } from '@theme-ui/color';
 
 /**
  * Buttons allow users to take actions, and make choices, with a single tap
  */
 export default function Button({
   children,
-  onClick = (e) => {
-    e.preventDefault();
-  },
-  variant = 'default',
+  color = 'default',
+  onClick,
+  variant = 'contained',
+  disabled = false,
 }) {
   return (
-    <TuiButton onClick={onClick} variant={variant}>
-      {children}
+    <TuiButton
+      disabled={disabled}
+      onClick={onClick}
+      sx={{
+        alignItems: 'center',
+        borderRadius: 2,
+        cursor: 'pointer',
+        display: 'flex',
+        justifyContent: 'center',
+        minHeight: 6,
+        paddingLeft: 3,
+        paddingRight: 3,
+        '&:disabled': {
+          backgroundColor: lighten('muted', 0.075),
+          color: alpha('text', 0.4),
+          cursor: 'default',
+          boxShadow: 0,
+        },
+        variant: `buttons.${variant}.${color}`,
+      }}
+    >
+      <Text variant="button">{children}</Text>
     </TuiButton>
   );
 }
 
 Button.propTypes = {
+  /**
+   * The color of the button
+   */
+  color: PropTypes.oneOf([
+    'default',
+    'primary',
+    'secondary',
+    'warning',
+    'error',
+  ]),
+
+  /**
+   * Whether or not the button is disabled
+   */
+  disabled: PropTypes.bool,
+
   /**
    * Content of the button
    */
@@ -28,10 +65,10 @@ Button.propTypes = {
   /**
    * Click handler to respond to user action
    */
-  onClick: PropTypes.func,
+  onClick: PropTypes.func.isRequired,
 
   /**
    * Style presets applied to the component
    */
-  variant: PropTypes.oneOf(['default', 'primary']),
+  variant: PropTypes.oneOf(['contained', 'text', 'outlined']),
 };
--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -1,24 +1,84 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
-import { lorem } from 'faker';
 
 import Button from './Button';
+
+function getButtons({ variant, children, disabled = false, onClick }) {
+  const buttons = ['default', 'primary', 'secondary', 'warning', 'error'].map(
+    (color) => (
+      <Button
+        key={`${color}-${variant}`}
+        color={color}
+        disabled={disabled}
+        onClick={onClick}
+        variant={variant}
+      >
+        {children}
+      </Button>
+    )
+  );
+
+  return [
+    ...buttons,
+    <Button
+      key={`${variant}-disabled`}
+      color={'default'}
+      disabled={true}
+      onClick={onClick}
+      variant={variant}
+    >
+      {children}
+    </Button>,
+  ];
+}
 
 export default {
   title: 'Components/Button',
   component: Button,
-  args: { label: lorem.words(2) },
-  argTypes: { onClick: { action: 'clicked' } },
+  args: {
+    children: 'Button',
+    color: 'default',
+    disabled: false,
+    variant: 'contained',
+  },
+  parameters: { actions: { argTypesRegex: '^on.*' } },
+  decorators: [
+    (story) => (
+      <div
+        style={{
+          display: 'grid',
+          gridGap: '16px',
+          gridTemplateColumns: 'repeat(6, auto)',
+        }}
+      >
+        {story()}
+      </div>
+    ),
+  ],
 };
 
-export const Default = ({ onClick, label }) => (
-  <Button onClick={onClick} variant={'default'}>
-    {label}
-  </Button>
-);
+export const Default = ({ onClick, children, variant, disabled, color }) => {
+  return (
+    <Button
+      color={color}
+      disabled={disabled}
+      onClick={onClick}
+      variant={variant}
+    >
+      {children}
+    </Button>
+  );
+};
 
-export const Primary = ({ onClick, label }) => (
-  <Button onClick={onClick} variant="primary">
-    {label}
-  </Button>
-);
+Default.story = {
+  decorators: [],
+};
+
+export const Contained = ({ onClick, children }) =>
+  getButtons({ variant: 'contained', children, onClick });
+
+export const Text = ({ onClick, children }) =>
+  getButtons({ variant: 'text', children, onClick });
+
+export const Warning = ({ onClick, children }) =>
+  getButtons({ variant: 'outlined', children, onClick });
